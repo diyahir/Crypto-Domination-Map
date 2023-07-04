99b6b77bd5e50bd684129e39503389691ff3d503
diyahir <diyahirc@gmail.com>
2023-07-04 14:28:16 +0900

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import { Code, HStack, Spacer, Text } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "../ColorModeSwitcher";
+import { parseLargeNumber } from "../utils/formating";
 
 export const Header = () => {
+  const fdv = 31000 * 21e6;
   return (
     <HStack w="100%" pr={15}>
       <Text fontSize={"2xl"}>
-        <Code fontSize={"xl"}>Bitcoin Dominance at $31,000</Code>
+        <Code fontSize={"xl"}>
+          Bitcoin Dominance at $31,000 - FDV ${parseLargeNumber(fdv.toString())}
+        </Code>
       </Text>
       <Spacer />
       <ColorModeSwitcher justifySelf="flex-end" mr={"40px"} />
